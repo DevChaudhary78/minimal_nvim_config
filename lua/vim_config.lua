cbd35a0b54ac7b7b8d07b172fec3f8ab83625fe3
Dev Chaudhary <devchaudhary.dc84.dc@gmail.com>
2023-12-17 19:13:59 -0500

    added bufferline, and nvimdap: for debugging and tabulation

--- lua/vim_config.lua
+++ lua/vim_config.lua
@@ -5,12 +5,12 @@ vim.opt.relativenumber = true
 vim.opt.number = true
 
 -- Set indent options
-vim.o.shiftwidth = 4
-vim.o.softtabstop = 4
-vim.o.tabstop = 4
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.tabstop = 2
 vim.o.expandtab = true
 
--- Set autoindent and smartindent 
+-- Set autoindent and smartindent
 vim.o.autoindent = true
 vim.o.smartindent = true
 
